d23630fd7efb7acd3b5a162d92f137a5c396bbca
Raul Costa <raulbrennersc@gmail.com>
2023-12-02 16:29:44 -0300

    moving echo statements inside if blocks

--- install.sh
+++ install.sh
@@ -19,8 +19,8 @@ mkdir -p ${HOME}/.config/
 cp -r $(pwd)/dotfiles/nvim ${HOME}/.config
 cp $(pwd)/dotfiles/zsh/.zshrc ${HOME}/.zshrc
 
-echo "Generating ssh keys"
 if ! [ -f "${HOME}/.ssh/id_ed25519" ]; then
+  echo "Generating ssh keys"
   ssh-keygen -t ed25519 -f ${HOME}/.ssh/id_ed25519 -q -P ""
   eval "$(ssh-agent -s)"
   ssh-add ${HOME}/.ssh/id_ed25519
@@ -29,72 +29,64 @@ fi
 echo "Installing build-essential"
 sudo apt install build-essential -y
 
-echo  "Install nala"
-if ! command -v nala &> /dev/null
-then
-    sudo apt install nala -y
+if ! command -v nala &> /dev/null then
+  echo  "Install nala"
+  sudo apt install nala -y
 fi
 
-echo  "Install zsh"
-if ! command -v zsh &> /dev/null
-then
+if ! command -v zsh &> /dev/null then
+  echo  "Install zsh"
     sudo nala install zsh -y
 fi
 
-echo "Install Homebrew"
 if ! [ -d "/home/linuxbrew/" ]; then
+  echo "Install Homebrew"
   NONINTERACTIVE=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
   eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
 fi
 
+if ! command -v curl &> /dev/null then
 echo "Install curl"
-if ! command -v curl &> /dev/null
-then
     sudo nala install curl -y
 fi
 
+if ! command -v unzip &> /dev/null then
 echo "Install unzip"
-if ! command -v unzip &> /dev/null
-then
     sudo nala install unzip -y
 fi
 
-echo "Install oh my zsh"
 if ! [ -d "${HOME}/.oh-my-zsh" ]; then
+  echo "Install oh my zsh"
   sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 fi
 
-echo "Install nvm"
-if ! command -v nvm &> /dev/null
-then
+if ! command -v nvm &> /dev/null then
+  echo "Install nvm"
   PROFILE=/dev/null bash -c 'curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.5/install.sh | bash'
   source ~/.nvm/nvm.sh
 fi
 
-echo "Install Meslo NF"
 if ! [ -d "${HOME}/.fonts/Meslo" ]; then
+  echo "Install Meslo NF"
   wget -P ${HOME}/.fonts/ https://github.com/ryanoasis/nerd-fonts/releases/latest/download/Meslo.zip
   unzip ${HOME}/.fonts/Meslo.zip -d ${HOME}/.fonts/Meslo
   rm -rf ${HOME}/.fonts/Meslo.zip
 fi
 
-echo "Install ripgrep (used by nvim)"
-if ! command -v ripgrep &> /dev/null
-then
+if ! command -v ripgrep &> /dev/null then
+  echo "Install ripgrep (used by nvim)"
   brew install ripgrep
 fi
 
-echo "Install nvim"
-if ! command -v nvim &> /dev/null
-then
+if ! command -v nvim &> /dev/null then
+  echo "Install nvim"
   brew install nvim
   nvim --headless +qa
   nvim --headless "+Lazy! sync" +qa
 fi
 
-echo "Install docker ce"
-if ! command -v docker &> /dev/null
-then
+if ! command -v docker &> /dev/null then
+  echo "Install docker ce"
   curl -fsSL https://get.docker.com -o- | sh
 fi
 
