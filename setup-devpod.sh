1f08cd5739a4755b4a6619f3345dd93838a08271
Raul Costa <raulbrennersc@gmail.com>
2025-02-15 22:50:11 -0300

    add zsh and oh-my-zsh to devcontainer setup

--- setup-devpod.sh
+++ setup-devpod.sh
@@ -12,6 +12,10 @@ ln -s ${HOME}/dotfiles/.zshrc ${HOME}/.zshrc
 ln -s ${HOME}/dotfiles/.gitconfig ${HOME}/.gitconfig
 ln -s ${HOME}/dotfiles/.tmux.conf ${HOME}/.tmux.conf
 
+sudo apt update
+sudo apt install zsh -y
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
 eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
 brew install fzf ripgrep neovim tmux oh-my-posh
 
