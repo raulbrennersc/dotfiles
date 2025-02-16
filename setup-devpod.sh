7964d377a9d109ab4882a730db80ebd9c50583f8
Raul Costa <raulbrennersc@gmail.com>
2025-02-15 22:08:07 -0300

    fix devpod stup

--- setup-devpod.sh
+++ setup-devpod.sh
@@ -3,9 +3,14 @@ export XDG_CONFIG_HOME="$HOME"/.config
 
 mkdir -p ${HOME}/.config
 ln -s ${HOME}/dotfiles/.config/nvim ${HOME}/.config/nvim
+
+rm -rf ${HOME}/.zshrc
+rm -rf ${HOME}/.gitconfig
+rm -rf ${HOME}/.tmux.conf
+
 ln -s ${HOME}/dotfiles/.zshrc ${HOME}/.zshrc
 ln -s ${HOME}/dotfiles/.gitconfig ${HOME}/.gitconfig
-ln -s ${HOME}/dotfiles/.tmux.conf ${HOME}/dotfiles/.tmux.conf
+ln -s ${HOME}/dotfiles/.tmux.conf ${HOME}/.tmux.conf
 
 eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
 brew install fzf ripgrep neovim tmux oh-my-posh
