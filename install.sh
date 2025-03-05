8794386b569d1321156902e8ae5905e5a98a6117
Raul Costa <raulbrennersc@gmail.com>
2025-03-04 21:02:11 -0300

    change paths and add git clone

--- install.sh
+++ install.sh
@@ -3,52 +3,52 @@
 set -e
 set -f
 
-DOTFILES_USER=${USER}
+echo "Install packages"
+sudo pacman -S git base-devel neovim flatpak tmux zsh curl openssh \
+  alacritty docker docker-compose ddcutil xclip spotify-launcher \
+  fastfetch transmission-gtk vlc -y
+
+if ! [ -d "~/dotfiles" ]; then
+  git clone https://github.com/raulbrennersc/dotfiles.git ~/dotfiles
+  cd ~/dotfiles
+  git remote set-url origin git@github.com:raulbrennersc/dotfiles.git
+fi
 
 echo "Create symlinks for dotfiles"
-mkdir -p ${HOME}/.config
-mkdir -p ${HOME}/.config/VSCodium/User/
-mkdir -p ${HOME}/.config/alacritty
-ln -s ${HOME}/dotfiles/.config/solaar ${HOME}/.config/solaar
-ln -s ${HOME}/dotfiles/.config/nvim ${HOME}/.config/nvim
-ln -s ${HOME}/dotfiles/.gitconfig ${HOME}/.gitconfig
-ln -s ${HOME}/dotfiles/.tmux.conf ${HOME}/.tmux.conf
-ln -s ${HOME}/dotfiles/.config/alacritty/alacritty.toml ${HOME}/.config/alacritty/alacritty.toml
+mkdir -p ~/.config
+ln -s ~/dotfiles/.config/nvim ~/.config/nvim
+ln -s ~/dotfiles/.gitconfig ~/.gitconfig
+ln -s ~/dotfiles/.tmux.conf ~/.tmux.conf
+ln -s ~/dotfiles/.config/alacritty ~/.config/alacritty
 
 echo "Generate ssh keys"
-ssh-keygen -t ed25519 -f ${HOME}/.ssh/id_ed25519 -q -P ""
+ssh-keygen -t ed25519 -f ~/.ssh/id_ed25519 -q -P ""
 eval "$(ssh-agent -s)"
-ssh-add ${HOME}/.ssh/id_ed25519
-
-echo "Install packages"
-sudo pacman -S neovim flatpak solaar tmux zsh curl openssh alacritty docker docker-compose ripgrep ddcutil -y
+ssh-add ~/.ssh/id_ed25519
 
 echo "Install yay"
 git clone https://aur.archlinux.org/yay.git
 cd yay
 makepkg -si
 
-echo "Enable Solaar"
-sudo setfacl -m u:${DOTFILES_USER}:rw /dev/uinput
-
 echo "Enable flathub"
 flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 
-echo "Install Zen"
-flatpak install app.zen_browser.zen io.github.alainm23.planify -y
+echo "Install flatpaks"
+flatpak install app.zen_browser.zen io.github.alainm23.planify io.dbeaver.DBeaverCommunity -y
 
-mkdir -p ${HOME}/.fonts
+mkdir -p ~/.fonts
 echo "Install Meslo Nerd Font"
-wget -P ${HOME}/.fonts/ https://github.com/ryanoasis/nerd-fonts/releases/latest/download/Meslo.zip
-unzip ${HOME}/.fonts/Meslo.zip -d ${HOME}/.fonts/Meslo
-rm -rf ${HOME}/.fonts/Meslo.zip
+wget -P ~/.fonts/ https://github.com/ryanoasis/nerd-fonts/releases/latest/download/Meslo.zip
+unzip ~/.fonts/Meslo.zip -d ~/.fonts/Meslo
+rm -rf ~/.fonts/Meslo.zip
 
 echo "Install FiraCode Nerd Font"
-wget -P ${HOME}/.fonts/ https://github.com/ryanoasis/nerd-fonts/releases/latest/download/FiraCode.zip
-unzip ${HOME}/.fonts/FiraCode.zip -d ${HOME}/.fonts/FiraCode
-rm -rf ${HOME}/.fonts/FiraCode.zip
+wget -P ~/.fonts/ https://github.com/ryanoasis/nerd-fonts/releases/latest/download/FiraCode.zip
+unzip ~/.fonts/FiraCode.zip -d ~/.fonts/FiraCode
+rm -rf ~/.fonts/FiraCode.zip
 
-sudo chsh $DOTFILES_USER -s $(which zsh)
+sudo chsh $USER -s $(which zsh)
 
 echo "Install oh-my-zsh"
 curl -L http://install.ohmyz.sh | sh
@@ -56,16 +56,16 @@ curl -L http://install.ohmyz.sh | sh
 echo "Install oh-my-posh"
 curl -s https://ohmyposh.dev/install.sh | bash -s
 
-rm -rf ${HOME}/.zshrc
-ln -s ${HOME}/dotfiles/.zshrc ${HOME}/.zshrc
+rm -rf ~/.zshrc
+ln -s ~/dotfiles/.zshrc ~/.zshrc
 
 echo "Install DevPod"
 curl -L -o devpod "https://github.com/loft-sh/devpod/releases/latest/download/devpod-linux-amd64" && sudo install -c -m 0755 devpod /usr/local/bin && rm -f devpod
 
-sudo usermod -aG docker $DOTFILES_USER
+sudo usermod -aG docker $USER
 newgrp docker
 sudo systemctl enable --now docker.socket
-sudo systemctl enable --now sshd
-systemctl --user enable --now gcr-ssh-agent.socket
+#sudo systemctl enable --now sshd
+#systemctl --user enable --now gcr-ssh-agent.socket
 
 sudo reboot
