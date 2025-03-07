343bf55674511823e537d9a9ac4c6e52ed3bae0f
Raul Costa <raulbrennersc@gmail.com>
2025-03-07 01:39:47 -0300

    change colorschemes

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -1,21 +1,20 @@
 return {
   {
-    "projekt0n/github-nvim-theme",
-    name = "github-theme",
-    lazy = false, -- make sure we load this during startup if it is your main colorscheme
-    priority = 1000, -- make sure to load this before all the other start plugins
+    "catppuccin/nvim",
+    lazy = false,
+    name = "catppuccin",
+    priority = 1000,
     config = function()
-      require("github-theme").setup({
-        options = {
-          transparent = true,
-        },
+      require("catppuccin").setup({
+        transparent_background = true,
+        flavour = "mocha",
       })
     end,
   },
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "github_dark_default",
+      colorscheme = "catppuccin",
     },
   },
 }
--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -6,12 +6,14 @@ if wezterm.config_builder then
 	config = wezterm.config_builder()
 end
 
-config.color_scheme = "Wez"
+config.color_scheme = "Catppuccin Mocha"
 config.font = wezterm.font_with_fallback({
 	{ family = "FiraCode Nerd Font", scale = 1.5 },
 })
-config.window_background_opacity = 0.8
+config.window_background_opacity = 0.7
 config.window_decorations = "RESIZE"
 config.enable_tab_bar = false
-
+config.colors = {
+	background = "#000000",
+}
 return config
