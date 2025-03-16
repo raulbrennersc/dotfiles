364be568e893692592954c0618491bf66fe523e3
Raul Costa <raulbrennersc@gmail.com>
2025-03-16 10:13:18 -0300

    add tmux plugins

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -15,17 +15,18 @@ config.font_size = 15
 config.window_background_opacity = 0.8
 config.window_decorations = "RESIZE"
 config.use_fancy_tab_bar = false
+config.hide_tab_bar_if_only_one_tab = true
 config.tab_bar_at_bottom = true
 config.window_padding = {
-	top = "0.4cell",
-	bottom = 0,
-	left = 0,
-	right = 0,
+	top = 0,
+	bottom = "0.4cell",
+	-- left = 0,
+	-- right = 0,
 }
 config.colors = {
 	background = "black",
 	tab_bar = {
-		background = "transparent",
+		-- background = "transparent",
 		active_tab = {
 			bg_color = "#89b4fa",
 			fg_color = "#181825",
@@ -34,101 +35,101 @@ config.colors = {
 	},
 }
 
-config.pane_focus_follows_mouse = true
+-- config.pane_focus_follows_mouse = true
 config.scrollback_lines = 5000
 
-config.leader = {
-	key = "a",
-	mods = "CTRL",
-	timeout_milliseconds = 2000,
-}
-
-config.keys = {
-	{
-		key = "f",
-		mods = "ALT",
-		action = act.TogglePaneZoomState,
-	},
-	{
-		key = "c",
-		mods = "LEADER",
-		action = act.SpawnTab("CurrentPaneDomain"),
-	},
-	{
-		key = "n",
-		mods = "LEADER",
-		action = wezterm.action.ActivateTabRelative(1),
-	},
-	{
-		key = "p",
-		mods = "LEADER",
-		action = wezterm.action.ActivateTabRelative(-1),
-	},
-	{
-		key = ",",
-		mods = "LEADER",
-		action = act.PromptInputLine({
-			description = "Enter new name for tab",
-			action = wezterm.action_callback(function(window, _, line)
-				if line then
-					window:active_tab():set_title(line)
-				end
-			end),
-		}),
-	},
-	{
-		key = "w",
-		mods = "LEADER",
-		action = act.ShowTabNavigator,
-	},
-	{
-		key = "&",
-		mods = "LEADER|SHIFT",
-		action = act.CloseCurrentTab({ confirm = true }),
-	},
-	{
-		key = "=",
-		mods = "LEADER",
-		action = act.SplitPane({
-			direction = "Right",
-			size = { Percent = 50 },
-		}),
-	},
-	{
-		key = "-",
-		mods = "LEADER",
-		action = act.SplitPane({
-			direction = "Down",
-			size = { Percent = 50 },
-		}),
-	},
-	{
-		key = ";",
-		mods = "LEADER",
-		action = act.ActivatePaneDirection("Prev"),
-	},
-	{
-		key = "o",
-		mods = "LEADER",
-		action = act.ActivatePaneDirection("Next"),
-	},
-	{
-		key = "$",
-		mods = "LEADER|SHIFT",
-		action = act.PromptInputLine({
-			description = "Enter new name for session",
-			action = wezterm.action_callback(function(window, _, line)
-				if line then
-					mux.rename_workspace(window:mux_window():get_workspace(), line)
-				end
-			end),
-		}),
-	},
-	{
-		key = "s",
-		mods = "LEADER",
-		action = act.ShowLauncherArgs({ flags = "WORKSPACES" }),
-	},
-}
-
+-- config.leader = {
+-- 	key = "a",
+-- 	mods = "CTRL",
+-- 	timeout_milliseconds = 2000,
+-- }
+--
+-- config.keys = {
+-- 	{
+-- 		key = "f",
+-- 		mods = "ALT",
+-- 		action = act.TogglePaneZoomState,
+-- 	},
+-- 	{
+-- 		key = "c",
+-- 		mods = "LEADER",
+-- 		action = act.SpawnTab("CurrentPaneDomain"),
+-- 	},
+-- 	{
+-- 		key = "n",
+-- 		mods = "LEADER",
+-- 		action = wezterm.action.ActivateTabRelative(1),
+-- 	},
+-- 	{
+-- 		key = "p",
+-- 		mods = "LEADER",
+-- 		action = wezterm.action.ActivateTabRelative(-1),
+-- 	},
+-- 	{
+-- 		key = ",",
+-- 		mods = "LEADER",
+-- 		action = act.PromptInputLine({
+-- 			description = "Enter new name for tab",
+-- 			action = wezterm.action_callback(function(window, _, line)
+-- 				if line then
+-- 					window:active_tab():set_title(line)
+-- 				end
+-- 			end),
+-- 		}),
+-- 	},
+-- 	{
+-- 		key = "w",
+-- 		mods = "LEADER",
+-- 		action = act.ShowTabNavigator,
+-- 	},
+-- 	{
+-- 		key = "&",
+-- 		mods = "LEADER|SHIFT",
+-- 		action = act.CloseCurrentTab({ confirm = true }),
+-- 	},
+-- 	{
+-- 		key = "=",
+-- 		mods = "LEADER",
+-- 		action = act.SplitPane({
+-- 			direction = "Right",
+-- 			size = { Percent = 50 },
+-- 		}),
+-- 	},
+-- 	{
+-- 		key = "-",
+-- 		mods = "LEADER",
+-- 		action = act.SplitPane({
+-- 			direction = "Down",
+-- 			size = { Percent = 50 },
+-- 		}),
+-- 	},
+-- 	{
+-- 		key = ";",
+-- 		mods = "LEADER",
+-- 		action = act.ActivatePaneDirection("Prev"),
+-- 	},
+-- 	{
+-- 		key = "o",
+-- 		mods = "LEADER",
+-- 		action = act.ActivatePaneDirection("Next"),
+-- 	},
+-- 	{
+-- 		key = "$",
+-- 		mods = "LEADER|SHIFT",
+-- 		action = act.PromptInputLine({
+-- 			description = "Enter new name for session",
+-- 			action = wezterm.action_callback(function(window, _, line)
+-- 				if line then
+-- 					mux.rename_workspace(window:mux_window():get_workspace(), line)
+-- 				end
+-- 			end),
+-- 		}),
+-- 	},
+-- 	{
+-- 		key = "s",
+-- 		mods = "LEADER",
+-- 		action = act.ShowLauncherArgs({ flags = "WORKSPACES" }),
+-- 	},
+-- }
+--
 return config
