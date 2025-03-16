74e5a5dee9fb3b05e64ac3dabc699314eae392a7
Raul Costa <raulbrennersc@gmail.com>
2025-03-16 10:36:48 -0300

    enable osc52 clipboard

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -5,3 +5,14 @@ local path = vim.fn.fnamemodify(vim.fn.getcwd(), ":~")
 -- vim.opt.titlestring = "\u{e7c5} " .. path
 vim.opt.titlestring = "\u{f36f} " .. path
 require("config.lazy")
+vim.g.clipboard = {
+  name = "OSC 52",
+  copy = {
+    ["+"] = require("vim.ui.clipboard.osc52").copy("+"),
+    ["*"] = require("vim.ui.clipboard.osc52").copy("*"),
+  },
+  paste = {
+    ["+"] = require("vim.ui.clipboard.osc52").paste("+"),
+    ["*"] = require("vim.ui.clipboard.osc52").paste("*"),
+  },
+}
