98351730784f3c3c218886b8d3983248971d6099
Raul Costa <raulbrennersc@gmail.com>
2025-04-29 17:05:36 -0300

    fix debian packages

--- install.sh
+++ install.sh
@@ -3,18 +3,42 @@
 set -e
 set -f
 
-echo "Install ArchLinux packages"
-sudo pacman -S --noconfirm git base-devel flatpak zsh curl openssh \
-  docker ddcutil xclip fastfetch transmission-gtk vlc unzip neovim \
-  cmatrix fd curl wget nerd-fonts ttf-font-awesome solaar cargo \
-  spotify-launcher steam papirus-icon-theme gnome-themes-extra \
-  pacman-contrib networkmanager-openvpn xorg-xhost libnma
+echo "Install Debian packages"
+sudo apt-get update
+sudo apt-get install -y git build-essential flatpak zsh curl openssh-server \
+  ddcutil xclip transmission vlc unzip cmatrix fd-find curl systemd-resolved \
+  solaar cargo papirus-icon-theme gnome-themes-extra fastfetch cava \
+  network-manager-openvpn xorg-xhost libnma-dev ca-certificates vim
 
-echo "Install yay"
-git clone https://aur.archlinux.org/yay.git
-cd yay
-makepkg -si
-cd
+echo "Install docker"
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+echo "deb [arch=amd64 signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/debian trixie stable" |
+  sudo tee /etc/apt/sources.list.d/docker.list >/dev/null
+sudo apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+echo "Install Wezterm"
+wget https://github.com/wezterm/wezterm/releases/download/nightly/wezterm-nightly.Debian12.deb -O wezterm.deb
+sudo apt-get install ./wezterm.deb -y
+rm -rf ./wezterm.deb
+
+echo "Install Google Chrome"
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb -O chrome.deb
+sudo apt-get install ./chrome.deb -y
+rm -rf ./chrome.deb
+
+echo "Install Spotify"
+curl -sS https://download.spotify.com/debian/pubkey_C85668DF69375001.gpg | sudo gpg --dearmor --yes -o /etc/apt/trusted.gpg.d/spotify.gpg
+echo "deb https://repository.spotify.com stable non-free" | sudo tee /etc/apt/sources.list.d/spotify.list
+sudo apt-get update && sudo apt-get install spotify-client
+
+echo "Install Steam"
+wget https://cdn.fastly.steamstatic.com/client/installer/steam.deb -O steam.deb
+sudo apt-get install ./steam.deb -y
+rm -rf ./steam.deb
 
 echo "Generate ssh keys and config"
 ssh-keygen -t ed25519 -f ~/.ssh/id_ed25519 -q -P ""
@@ -35,8 +59,6 @@ ln -s ~/dotfiles/.config/nvim ~/.config/nvim
 ln -s ~/dotfiles/.gitconfig ~/.gitconfig
 ln -s ~/dotfiles/.config/wezterm ~/.config/wezterm
 ln -s ~/dotfiles/.config/solaar ~/.config/solaar
-ln -s ~/dotfiles/.config/sway ~/.config/sway
-ln -s ~/dotfiles/.config/waybar ~/.config/waybar
 ln -s ~/dotfiles/.config/cava ~/.config/cava
 ln -s ~/dotfiles/.config/dconf/user.d ~/.config/dconf/user.d
 cp ~/dotfiles/.config/environment.d/mangohud.conf ~/.config/environment.d/mangohud.conf
@@ -63,7 +85,7 @@ echo "Apply GNOME customization"
 dconf compile ~/.config/dconf/user ~/.config/dconf/user.d
 sudo dconf update
 
-sudo chsh $USER -s $(which zsh)
+sudo chsh $USER -s /usr/bin/zsh
 
 echo "Install oh-my-zsh"
 curl -L http://install.ohmyz.sh | sh
@@ -79,7 +101,6 @@ sudo usermod -aG docker $USER
 newgrp docker
 
 sudo systemctl enable systemd-resolved.service
-sudo systemctl enable sshd
 sudo systemctl enable docker.socket
 sudo systemctl enable docker.service
 systemctl --user enable gcr-ssh-agent.socket
