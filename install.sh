6ce21f5dc9e89286c719eb5948ca8201f744ae8e
Raul Costa <raulbrennersc@gmail.com>
2025-06-01 12:07:44 -0300

    remove whatsapp flatpak (use chromium pwa now)

--- install.sh
+++ install.sh
@@ -46,7 +46,7 @@ echo "Enable flathub"
 sudo flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 
 echo "Install flatpaks"
-flatpak install -y app.zen_browser.zen re.sonny.Junction com.rtosta.zapzap
+flatpak install -y app.zen_browser.zen re.sonny.Junction
 
 echo "Set default browser"
 xdg-settings set default-web-browser re.sonny.Junction.desktop
