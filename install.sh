96a0c1988a2b092ad2c4bfcc834808d1ff6b93e7
Raul Costa <raulbrennersc@gmail.com>
2025-06-08 19:11:37 -0300

    change mouse LED

--- install.sh
+++ install.sh
@@ -34,6 +34,7 @@ ln -s ~/dotfiles/.config/wezterm ~/.config/wezterm
 ln -s ~/dotfiles/.config/alacritty ~/.config/alacritty
 ln -s ~/dotfiles/.config/solaar ~/.config/solaar
 ln -s ~/dotfiles/.config/cava ~/.config/cava
+ln -s ~/dotfiles/.config/MangoHud ~/.config/MangoHud
 ln -s ~/dotfiles/.config/dconf/user.d ~/.config/dconf/user.d
 cp ~/dotfiles/.config/environment.d/mangohud.conf ~/.config/environment.d/mangohud.conf
 cp ~/dotfiles/.docker/config.json ~/.docker/config.json
