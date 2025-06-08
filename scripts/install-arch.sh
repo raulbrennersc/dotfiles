00140f7c1ad24f2e6d53568e29635b1b04578f8a
Raul Costa <raulbrennersc@gmail.com>
2025-06-08 15:33:22 -0300

    fix arch install

--- scripts/install-arch.sh
+++ scripts/install-arch.sh
@@ -5,9 +5,9 @@ sudo pacman -Syu --noconfirm git base-devel flatpak curl openssh \
   ddcutil xclip vlc unzip cmatrix fd curl solaar fastfetch cava \
   bash-completion networkmanager-openvpn vim qbittorrent tmux \
   chromium alacritty libnma ripgrep mangohud sqlite fuse2 \
-  neovim librewolf docker papirus-icon-theme wget nerd-fonts \
+  neovim docker papirus-icon-theme wget nerd-fonts discord \
   ttf-font-awesome spotify-launcher xorg-xhost pacman-contrib \
-  discord dbeaver ttf-nerd-fonts-symbols-mono \
+  dbeaver ttf-nerd-fonts-symbols-mono \
   gnome-console gnome-themes-extra gnome-system-monitor \
   gnome-shell-extension-appindicator
 
