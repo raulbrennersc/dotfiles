f235e371691b9f39824939a22f84fcc6deead64d
Raul Costa <raulbrennersc@gmail.com>
2026-04-10 20:11:24 -0300

    wezterm tokyonight theme

--- .config/wezterm/themes/tokyonight.lua
+++ .config/wezterm/themes/tokyonight.lua
@@ -1,10 +1,10 @@
 local wezterm = require("wezterm")
-local color_scheme = wezterm.color.get_builtin_schemes()["Tokyo Night"]
+local color_scheme = wezterm.color.get_builtin_schemes()["tokyonight_night"]
 local primary_color = "#82aaff"
 local inactive_color = "#3b4261"
 local background_color = color_scheme.background
 
 return {
-  builtin_color_scheme_name = "Tokyo Night",
+  builtin_color_scheme_name = "tokyonight_night",
   color_scheme = color_scheme,
 }
