bc89bf60de1344490e96cb44ce70df35ba2c72cf
Raul Costa <raulbrennersc@gmail.com>
2026-04-29 17:19:21 -0300

    hyprland customization

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -2,4 +2,12 @@ vim.pack.add({
   { src = "gh:folke/tokyonight.nvim", name = "tokyonight" },
 })
 
+require("tokyonight").setup({
+  -- transparent = true,
+  -- styles = {
+  --   sidebar = "transparent",
+  --   floats = "transparent",
+  -- },
+})
+
 vim.cmd([[colorscheme tokyonight-night]])
