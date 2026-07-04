a7b8ef8110d4b26da064f6c6bcaf1e9b8d7a66bb
Raul Costa <raulbrennersc@gmail.com>
2026-07-04 11:12:37 -0300

    add vm host

--- flake.nix
+++ flake.nix
@@ -37,6 +37,19 @@
           }
         ];
       };
+      raul-vm = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [
+          ./hosts/vm/configuration.nix
+          
+          home-manager.nixosModules.home-manager
+          {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.users.raul = import ./home.nix;
+          }
+        ];
+      };
     };
   };
 }
